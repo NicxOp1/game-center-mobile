13025378d6092a0990b1b60b5aa6485153b2ad6b
cornicheli <gabrielcornide.pg@gmail.com>
2022-12-22 06:31:06 -0300

    details, add video

--- src/Navigation/Drawer.js
+++ src/Navigation/Drawer.js
@@ -7,12 +7,12 @@ import Profile from "../Screen/Profile";
 import { StyleSheet, Text } from "react-native";
 import MenuButtonItem from "../Components/MenuButtonItem";
 
-import Store from "../Screen/Store";
+// import Store from "../Screen/Store";
 import AddGame from "../Screen/AddGame";
 import SignIn from "../Components/SignIn";
 import Library from "../Screen/Library";
 import Home from "../Screen/Home";
-
+import Details from "../Screen/Details";
 
 const DrawerNav = createDrawerNavigator();
 
@@ -20,12 +20,13 @@ export default function Drawer() {
   return (
     <>
       <DrawerNav.Navigator drawerContent={(props) => <MenuItems {...props} />}>
-      <DrawerNav.Screen name="Home" component={Home} />
+        <DrawerNav.Screen name="Home" component={Home} />
         <DrawerNav.Screen name="Profile" component={Profile} />
-        <DrawerNav.Screen name="Store" component={Store} />
+        {/* <DrawerNav.Screen name="Store" component={Store} /> */}
         <DrawerNav.Screen name="Join" component={SignIn} />
         <DrawerNav.Screen name="AddGame" component={AddGame} />
         <DrawerNav.Screen name="Library" component={Library} />
+        <DrawerNav.Screen name="Details" component={Details} />
       </DrawerNav.Navigator>
     </>
   );
@@ -42,10 +43,10 @@ const MenuItems = ({ navigation }) => {
           onPress={() => navigation.navigate("Profile")}
         />
 
-        <MenuButtonItem
+        {/* <MenuButtonItem
           text="Store"
           onPress={() => navigation.navigate("Store")}
-        />
+        /> */}
 
         <MenuButtonItem
           text="Library"
@@ -54,9 +55,14 @@ const MenuItems = ({ navigation }) => {
         <MenuButtonItem
           text="Home"
           onPress={() => navigation.navigate("Home")}
-         <MenuButtonItem
+        />
+        <MenuButtonItem
           text="Join"
           onPress={() => navigation.navigate("Join")}
+        />
+        <MenuButtonItem
+          text="Details"
+          onPress={() => navigation.navigate("Details")}
         />
       </DrawerContentScrollView>
     </>
